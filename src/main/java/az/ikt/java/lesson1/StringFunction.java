e9727aeb4d88de38478c17835c76ca7e8d7e52a0
Gulchohra Sultanova <115097212+Gulcohra18@users.noreply.github.com>
2022-11-15 23:27:01 +0400

    Update StringFunction.java

--- src/main/java/az/ikt/java/lesson1/StringFunction.java
+++ src/main/java/az/ikt/java/lesson1/StringFunction.java
@@ -12,8 +12,8 @@ public class StringFunction {
         System.out.println(a.toUpperCase());                     //  every letter are upper
         System.out.println(a.charAt(3));                         //  print n-th letter
         System.out.println(a.chars());                           //
-        System.out.println(a.codePointAt(4));               //  print n-th letter's ASCII code
-        System.out.println(a.codePointBefore(3));           //  print (n-1)-th letter's ASCII code
+        System.out.println(a.codePointAt(4));                    //  print n-th letter's ASCII code
+        System.out.println(a.codePointBefore(3));                //  print (n-1)-th letter's ASCII code
         System.out.println(a.codePointCount(0, 4));              //  count(int,int) ASCII
         System.out.println(b.compareTo(a));                      //  compare strings for UNICODE
         System.out.println(b.compareToIgnoreCase(a));            //  compare strings for UNICODE
@@ -21,11 +21,11 @@ public class StringFunction {
         System.out.println(a.contains(b));                       //  if b in a print(true) else print(false)
         System.out.println(a.contentEquals(b));                  //  if a==b print(true) else print(false)
         System.out.println(a.endsWith(b));                       //  a[n-1]==b print(true) else print(false)
-        System.out.println(a.repeat(5));                   //  repeat string for n time
+        System.out.println(a.repeat(5));                         //  repeat string for n time
         char c[] = a.toCharArray();
         for (int i = 0; i < a.length(); i++)
             System.out.println(c[i]);                            //  create array from string
-        System.out.println(a.replace('A','Z'));   //  replace(char,char)
+        System.out.println(a.replace('A','Z'));                  //  replace(char,char)
         System.out.println(a.trim());
     }
 }
